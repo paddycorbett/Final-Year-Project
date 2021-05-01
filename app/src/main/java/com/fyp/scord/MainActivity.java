f51d469b791b822c6f8a4870777530d6338425ed
paddycorbett <55563848+paddycorbett@users.noreply.github.com>
2021-05-01 14:59:47 +0100

    Ui menu option. Not yet finished

--- app/src/main/java/com/fyp/scord/MainActivity.java
+++ app/src/main/java/com/fyp/scord/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -93,11 +95,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
-
-
-
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dropdown_menu,menu);
+        return true;
+    }
 }
